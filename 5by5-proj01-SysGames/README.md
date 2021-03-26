9afcc8f9d8d7ffe64c1229a99c4fd8bf7fe9d8aa
Leonardo Larocca <leolrccv@gmail.com>
2021-03-26 09:42:38 -0300

    MVC

---
+++ 5by5-proj01-SysGames/README.md
@@ -0,0 +1,11 @@
+# 5by5-proj01-SysGames
+
+### Trello
+https://trello.com/b/VO3MZJ1H/sysgames
+
+### Diagrama de classes(Draw.io)
+![Sysgames (1)](https://user-images.githubusercontent.com/6529705/112632574-0d17fd00-8e17-11eb-9ed6-b4350e4980cc.png)
+https://drive.google.com/file/d/1iYDdHUvvmjcEvcPGgzYv5V99ezuPpe5I/view?usp=sharing
+
+### Diagrama do banco de dados
+![Diagrama_SysGames](https://user-images.githubusercontent.com/6529705/112631216-5a936a80-8e15-11eb-81d8-d7cfbff62281.png)
---
+++ 5by5-proj01-SysGames/SysGames/SysGames/Models/Carrinho.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace SysGames.Models
+{
+    [Table("Carrinhos")]
+    public class Carrinho
+	{
+        [Display(Name = "ID do Carrinho")]
+
+        public int CarrinhoID { get; set; }
+        /*
+        public int ItemID { get; set; }
+        public virtual List<ItemCarrinho> Produtos { get; set; }
+        */
+        public virtual Produto Produto { get; set; }
+        public virtual Cliente Cliente { get; set; }
+
+	}
+}
